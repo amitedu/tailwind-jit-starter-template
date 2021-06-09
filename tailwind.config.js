9d4210d2433ef5ee24c7933d9840363abeabae81
amit <amitedu21@gmail.com>
2021-06-09 16:55:59 +0530

    add navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,25 @@
 module.exports = {
-  purge: [
-      './public/**/*.html'
-  ],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+    purge: [
+        './public/**/*.html'
+    ],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Inter var'],
+                poppins: ['Poppins', 'sans-serif'],
+            },
+            colors: {
+                "theme-green": "#00b140",
+                "theme-aquamarine": "#44dbb2",
+                "theme-blue-deep": "#0a192f",
+                "theme-gray-light": "#ccd6f6",
+                "theme-gray-deep": "#8892b0",
+            }
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
 }
