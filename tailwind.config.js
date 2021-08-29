fddb8c242c99624cdde076343bcdd3dc9303fc3c
Amit <amitedu21@gmail.com>
2021-08-29 09:53:19 +0530

    fixing, optimizing for production

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,38 +1,34 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
-const colors = require('tailwindcss/colors')
+const colors = require("tailwindcss/colors");
 
 module.exports = {
-    purge: [
-        './public/**/*.html'
-    ],
-    darkMode: false, // or 'media' or 'class'
-    theme: {
-        screens: {
-            'xs': '475px',
-            ...defaultTheme.screens,
-        },
-        extend: {
-            fontFamily: {
-                sans: ['Inter var'],
-                poppins: ['Poppins', 'sans-serif'],
-            },
-            colors: {
-                "theme-green": "#00b140",
-                "theme-aquamarine": "#64ffda",
-                "theme-blue-deep": "#0a192f",
-                "theme-gray-light": "#ccd6f6",
-                "theme-gray-deep": "#8892b0",
-                "blueGray": colors.blueGray,
-                "emerald": colors.emerald,
-                "teal": colors.teal,
-                "cyan": colors.cyan,
-                "sky": colors.sky,
-            },
-            stroke: ['hover', 'focus'],
-        },
-    },
-    variants: {
-        extend: {},
-    },
-    plugins: [],
-}
+	purge: ["./public/**/*.html"],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		screens: {
+			xs: "475px",
+			...defaultTheme.screens,
+		},
+		extend: {
+			fontFamily: {
+				sans: ["Inter var"],
+				poppins: ["Poppins", "sans-serif"],
+			},
+			colors: {
+				"theme-green": "#00b140",
+				"theme-aquamarine": "#64ffda",
+				"theme-blue-deep": "#0a192f",
+				"theme-gray-light": "#ccd6f6",
+				"theme-gray-deep": "#8892b0",
+				emerald: colors.emerald,
+				teal: colors.teal,
+				cyan: colors.cyan,
+			},
+			stroke: ["hover", "focus"],
+		},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+};
